088bdad8983856c98ab3277a25315483ce338f07
Jeff Bencin <jeff@stackslabs.com>
2025-11-26 12:41:42 -0500

    Add `unused_token` lint and unit tests for unused FTs

--- components/clarity-repl/src/analysis/lints/mod.rs
+++ components/clarity-repl/src/analysis/lints/mod.rs
@@ -3,9 +3,11 @@ mod unused_const;
 mod unused_data_var;
 mod unused_map;
 mod unused_private_fn;
+mod unused_token;
 
 pub use noop::NoopChecker;
 pub use unused_const::UnusedConst;
 pub use unused_data_var::UnusedDataVar;
 pub use unused_map::UnusedMap;
 pub use unused_private_fn::UnusedPrivateFn;
+pub use unused_token::UnusedToken;
