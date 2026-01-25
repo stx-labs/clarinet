246da6744d070b58af14e59c2ff6083192cd06c1
Jeff Bencin <jeff@stackslabs.com>
2026-01-25 13:03:10 -0500

    Use `jemalloc` instead of default allocator on *nix platforms

--- components/clarinet-cli/src/bin.rs
+++ components/clarinet-cli/src/bin.rs
@@ -13,6 +13,13 @@ mod generate;
 mod lsp;
 
 use frontend::cli;
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+/// Enable jemalloc as the global allocator (except in Windows)
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 pub fn main() {
     cli::main();
