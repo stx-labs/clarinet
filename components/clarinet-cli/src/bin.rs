fb198d840abef91e08452c0c76e8c2c03c3436fb
Jeff Bencin <jeff@stackslabs.com>
2026-01-27 11:05:50 -0500

    perf: Improve Linux performance by using `jemalloc` instead of default allocator (#2149)
    * Use `jemalloc` instead of default allocator on *nix platforms
    
    * Disable `jemalloc` on MacOS targets
    
    * Run `cargo fmt-stacks`
    
    * Disable in Cargo.toml for MSVC and MacOS targets

--- components/clarinet-cli/src/bin.rs
+++ components/clarinet-cli/src/bin.rs
@@ -13,6 +13,16 @@ mod generate;
 mod lsp;
 
 use frontend::cli;
+#[cfg(not(any(target_env = "msvc", target_os = "macos")))]
+use tikv_jemallocator::Jemalloc;
+
+/// Enable jemalloc as the global allocator
+/// Disable for...
+///  - MSVC compiler: Not supported by `jemalloc`
+///  - MacOS: System allocator already based on jemalloc
+#[cfg(not(any(target_env = "msvc", target_os = "macos")))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 pub fn main() {
     cli::main();
