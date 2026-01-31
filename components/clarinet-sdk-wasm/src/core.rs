a92edbb63d6c37b0987c57e1b1b05187f82d2238
brady.ouren <233826532+brady-stacks@users.noreply.github.com>
2026-01-30 19:49:23 -0800

    fix: deploy contract respects cost tracking

--- components/clarinet-sdk-wasm/src/core.rs
+++ components/clarinet-sdk-wasm/src/core.rs
@@ -941,6 +941,7 @@ impl SDK {
             return Err(format!("Invalid sender address '{}'.", args.sender));
         }
 
+        let track_costs = self.options.track_costs;
         let execution = {
             let session = self.get_session_mut();
             if advance_chain_tip {
@@ -960,7 +961,7 @@ impl SDK {
                 epoch: Epoch::Specific(current_epoch),
             };
 
-            match session.deploy_contract(&contract, false, None) {
+            match session.deploy_contract(&contract, track_costs, None) {
                 Ok(res) => res,
                 Err(diagnostics) => {
                     let mut message = format!(
