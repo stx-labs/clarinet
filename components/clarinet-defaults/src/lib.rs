0ebd44f542e2cae8038ae78cf358f8efe75f3078
Hugo C <235113615+hugo-stacks@users.noreply.github.com>
2026-04-01 19:44:56 +0200

    feat: default to epoch 3.4

--- components/clarinet-defaults/src/lib.rs
+++ components/clarinet-defaults/src/lib.rs
@@ -1,5 +1,5 @@
 use clarity::types::StacksEpochId;
 use clarity::vm::ClarityVersion;
 
-pub const DEFAULT_EPOCH: StacksEpochId = StacksEpochId::Epoch33;
-pub const DEFAULT_CLARITY_VERSION: ClarityVersion = ClarityVersion::Clarity4;
+pub const DEFAULT_EPOCH: StacksEpochId = StacksEpochId::Epoch34;
+pub const DEFAULT_CLARITY_VERSION: ClarityVersion = ClarityVersion::Clarity5;
