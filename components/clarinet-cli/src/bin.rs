c8603b932b5b78e532f57e37ea5d3a1f841bce0b
Hugo <235113615+hugo-stacks@users.noreply.github.com>
2026-05-22 23:55:31 +0200

    ci: build for linux arm64 and generate checksum (#2407)

--- components/clarinet-cli/src/bin.rs
+++ components/clarinet-cli/src/bin.rs
@@ -1,12 +1,22 @@
 use clarinet_lib::frontend::cli;
-#[cfg(not(any(target_env = "msvc", target_os = "macos")))]
+#[cfg(not(any(
+    target_env = "msvc",
+    target_os = "macos",
+    all(target_arch = "aarch64", target_env = "musl")
+)))]
 use tikv_jemallocator::Jemalloc;
 
 /// Enable jemalloc as the global allocator
 /// Disable for...
 ///  - MSVC compiler: Not supported by `jemalloc`
 ///  - MacOS: System allocator already based on jemalloc
-#[cfg(not(any(target_env = "msvc", target_os = "macos")))]
+///  - aarch64-musl: `jemalloc` upstream has no atomics implementation for this
+///    target combo, falls back to musl's system allocator.
+#[cfg(not(any(
+    target_env = "msvc",
+    target_os = "macos",
+    all(target_arch = "aarch64", target_env = "musl")
+)))]
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
